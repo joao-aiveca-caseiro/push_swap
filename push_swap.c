dd8898e9295452d439fbce24fdda0ba16170fcb1
Joao Manuel Aiveca Caseiro <jaiveca-@c1r9s1.42lisboa.com>
2022-12-15 18:57:22 +0000

    instructions

--- push_swap.c
+++ push_swap.c
@@ -6,13 +6,34 @@
 /*   By: jaiveca- <jaiveca-@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 12:44:45 by jaiveca-          #+#    #+#             */
-/*   Updated: 2022/12/12 16:58:04 by jaiveca-         ###   ########.fr       */
+/*   Updated: 2022/12/15 18:55:42 by jaiveca-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	swap_pswap(t_list **stack, char c)
+{
+	void	*temp;
 
+	if (*stack)
+	{
+		temp = (*stack)->content;
+		(*stack)->content = (*stack)->next->content;
+		(*stack)->next->content = temp;
+		ft_printf("s%c\n", c);
+	}
+}
+
+void	push_pswap(t_list **src, t_list **dest, char c)
+{
+	if (*src)
+	{
+		ft_lstadd_front(dest, ft_lstnew((*src)->content));
+		*src = (*src)->next;
+		ft_printf("p%c\n", c);
+	}
+}
 
 int	main(void)
 {
@@ -25,10 +46,22 @@ int	main(void)
 	ft_lstadd_back(&stack_a, teste3);
 	ft_lstadd_back(&stack_a, teste4);
 
+	t_list *stack_b = NULL;
+
+	swap_pswap(&stack_a, 'a');
+	push_pswap(&stack_a, &stack_b, 'a');
+
+	ft_printf("Stack A\n");
 	while (stack_a != NULL)
 	{
 		ft_printf("%i\n", stack_a->content);
 		stack_a = stack_a->next;
+	}
+	ft_printf("Stack B\n");
+	while (stack_b != NULL)
+	{
+		ft_printf("%i\n", stack_b->content);
+		stack_b = stack_b->next;
 	}
 
 //	ft_lstiter(stacka, (void *)function);
