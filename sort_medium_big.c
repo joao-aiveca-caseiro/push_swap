79d50e638f291319d604e4234cd586470f821601
jaiveca- <jaiveca-@student.42lisboa.com>
2023-01-10 04:04:48 +0000

    a

--- sort_medium_big.c
+++ sort_medium_big.c
@@ -6,13 +6,13 @@
 /*   By: jaiveca- <jaiveca-@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 18:35:04 by jaiveca-          #+#    #+#             */
-/*   Updated: 2023/01/09 18:47:19 by jaiveca-         ###   ########.fr       */
+/*   Updated: 2023/01/10 04:04:12 by jaiveca-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	sort_big_pswap(t_list **a, t_list **b)
+/*void	sort_big_pswap(t_list **a, t_list **b)
 {
 	if (check_if_sorted(a) == 0)
 	{
@@ -26,4 +26,29 @@ void	sort_big_pswap(t_list **a, t_list **b)
 			push_pswap(b, a, 'a');
 		}
 	}
+}*/
+
+/*void	sort_big_pswap(t_list **a, t_list **b)
+{
+	
 }
+
+int	**create_chunks(t_list **a)
+{
+	int	stacksize;
+	int	total_chunks;
+	int	**chunks_array;
+
+	stacksize = ft_lstsize(a);
+	total_chunks = 0;
+
+	while (stacksize > 0)
+	{
+		stacksize = stacksize / 10;
+		total_chunks++;
+	}
+	chunks_array = malloc(sizeof(int *) * total_chunks);
+	if (!chunks_array)
+		return (NULL);
+	
+}*/
